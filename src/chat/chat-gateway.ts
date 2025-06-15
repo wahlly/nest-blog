301adfbb1814e1c7921b6c2d8f9f58356b53f1bd
wahllyWest <olaifaolawaleh@gmail.com>
2025-06-15 16:03:47 +0100

    event emitting

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -8,7 +8,7 @@ import {
 } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
-@WebSocketGateway()
+@WebSocketGateway(3200, {cors: {origin: "*"}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       @WebSocketServer() server: Server
       @SubscribeMessage("newMessage")
@@ -21,15 +21,22 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
             this.server.emit("response", "broadcasting...")
       }
 
-      handleConnection(client: any, ...args: any[]) {
+      handleConnection(client: Socket, ...args: any[]) {
             console.log("new user connected", client.id)
 
-            this.server.emit(`User joined the chat: ${client.id}`)
+            // this.server.emit(`User joined the chat: ${client.id}`)
+            client.broadcast.emit("user-joined", {
+                  message: `User joined the chat: ${client.id}`
+            })
       }
 
-      handleDisconnect(client: any) {
+      handleDisconnect(client: Socket) {
             console.log("user disconnected", client.id)
 
             this.server.emit(`User left the chat: ${client.id}`)
+
+            client.broadcast.emit("user-exit", {
+                  message: `User left the chat: ${client.id}`
+            })
       }
 }
